224e20528dbf7019fe9ecfa68b89d1b6a44da4f2
abdurrahmanaziz <uts.eksporyuk@gmail.com>
2026-07-12 17:46:38 +0700

    bump: WAFO_VERSION to 0.1.1 - prepare release with .zip asset

--- wa-form-optin.php
+++ wa-form-optin.php
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin constants.
  */
-define( 'WAFO_VERSION', '0.1.0' );
+define( 'WAFO_VERSION', '0.1.1' );
 define( 'WAFO_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WAFO_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'WAFO_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
